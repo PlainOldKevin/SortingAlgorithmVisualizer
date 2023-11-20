e7bb1a3ea908050ce2b08b9ab086665b84dc6fbb
unknown <kevinconwaylax@gmail.com>
2023-11-19 21:32:39 -0500

    created SortingPanel class for displaying array

---
+++ src/sorting/SortingPanel.java
@@ -0,0 +1,12 @@
+package sorting;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+// The JPanel for displaying the array
+public class SortingPanel {
+
+}
